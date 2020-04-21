7059024b0b144fbf213967144783e2c049f12d66
Brent F <feuerbacherb@gmail.com>
2020-04-21 15:26:44 -0500

    added error handling to fetch statement

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,15 +12,29 @@ var getUserRepos = function(user) {
 
    // make a request to the url
    fetch(apiUrl).then(function(response){
-      response.json().then(function(data) {
-         displayRepos(data, user);
-      });
-   });
+      // request was successful
+      if (response.ok) {
+         response.json().then(function(data) {
+            displayRepos(data, user);
+         });
+      } else {
+         alert("Error: " + response.statusText);
+      }
+   })
+   .catch(function(error) {
+      alert("Unable to connect to GitHub");
+   })
 }
 
 var displayRepos = function(repos, searchTerm) {
    console.log(repos);
    console.log(searchTerm);
+
+   // check to see if api returned any repos
+   if (repos.length === 0) {
+      repoContainerEl.textContent = "No repositories found.";
+      return;
+   } 
 
    // clear old content
    repoContainerEl.textContent = "";
