cb4628f0e854d6cbc7b7bc238e84d3944845a61b
Brent F <feuerbacherb@gmail.com>
2020-04-21 14:27:20 -0500

    added form informatio and capture data based on username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,9 @@
+// variables
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+// functions
 var getUserRepos = function(user) {
    // format the github api url
    var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +16,23 @@ var getUserRepos = function(user) {
    });
 }
 
-getUserRepos("feuerbacherb");
+
+// event handlers
+var formSubmitHandler = function(event) {
+   event.preventDefault();
+
+   // get value from input field
+   var username = nameInputEl.value.trim();
+
+   if (username) {
+      getUserRepos(username);
+      nameInputEl.value = "";
+   }
+   else {
+      alert("Please enter a GitHub username");
+   }
+}
+
+
+// event listeners
+userFormEl.addEventListener("submit", formSubmitHandler);
