c262eb7bc86c582e3d71219a7ab8a504ab4d4761
Brent Feuerbacher <feuerbacherb@gmail.com>
2024-03-14 14:57:50 -0500

    added error handling to fetch response

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,13 +9,29 @@ var getUserRepos = function (user) {
 
   // make a request to the url
   fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data, user);
+      });
+    } else {
+      if (response.status == 404) {
+        var statusText = "Not Found";
+      }
+      alert("Error: " + statusText);
+    }}.catch(function(error) {
+      // Notice this `.catch() getting chained onto the end of the `.then() method
+      alert("Unable to connect to GitHub");
     });
-  });
+  );
 };
 
 var displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerE1.textContent = "No repositories found.";
+    return;
+  }
+
   // clear old content
   repoContainerE1.textContent = "";
   repoSearchTerm.textContent = searchTerm;
