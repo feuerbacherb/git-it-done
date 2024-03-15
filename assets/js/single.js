d97d92fa9727c6889e689de22f47f65cd0bd2d06
Brent Feuerbacher <feuerbacherb@gmail.com>
2024-03-15 08:49:07 -0500

    added disclaimer if more than 30 issues and then provided a link to GitHub repository

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerE1 = document.querySelector("#issues-container");
+var limitWarningE1 = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +9,11 @@ var getRepoIssues = function (repo) {
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request");
@@ -50,6 +56,19 @@ var displayIssues = function (issues) {
 
     issueContainerE1.appendChild(issueE1);
   }
+};
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningE1.textContent = "More than 30 issues! ";
+
+  var linkE1 = document.createElement("a");
+  linkE1.textContent = "See More Issues on GitHub.com";
+  linkE1.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkE1.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningE1.appendChild(linkE1);
 };
 
 getRepoIssues("facebook/react");
